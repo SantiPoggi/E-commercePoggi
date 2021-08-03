5b7e5ade67d388c2f0403ea447fe629e9d4be6fa
SantiPoggi <santiagonpoggi@gmail.com>
2021-08-02 23:17:42 -0300

    "Tercer Commit"

--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -2,29 +2,73 @@ import React from 'react';
 import IMG from '../../images/milk stout.jpeg';
 
 export const ProductosLista = () => {
+    
     return (
         <>
-            <h1 className="title">Productos</h1>
+            <h1 className="title"> Productos</h1>
             <div className="productos">
-               <a href="/">
-                    <div className="producto">
-                        <img src={IMG} alt=""/>
+                <div className="producto">    
+                    <a href="q">
+                        <div className="producto_img">
+                            <img src={IMG} alt="" />
+                        </div>
+                    </a>
+                    <div className="producto_footer">
+                        <h1>Title</h1>
+                        <p>Categoria</p>
+                        <p className="price">$250</p>
                     </div>
-                </a>
-                <div className="producto_footer">
-                    <h1> Milk Stout </h1>
-                    <p> Manush </p>
-                    <p className="price">$250</p>
-                </div>
-                <div className="buttom">
-                    <button className="btn">
-                        Agregar al carro
-                    </button>
-                    <div>
-                        <a href="b" className="btn"> Vista </a>
+                    <div className="buttom">
+                        <button className="btn">Añadir</button>
+                        <div>
+                            <a href="w" className="">
+                                Vista
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div className="producto">    
+                    <a href="e">
+                        <div className="producto_img">
+                            <img src={IMG} alt="" />
+                        </div>
+                    </a>
+                    <div className="producto_footer">
+                        <h1>Title</h1>
+                        <p>Categoria</p>
+                        <p className="price">$250</p>
+                    </div>
+                    <div className="buttom">
+                        <button className="btn">Añadir</button>
+                        <div>
+                            <a href="r" className="">
+                                Vista
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="producto">    
+                    <a href="t">
+                        <div className="producto_img">
+                            <img src={IMG} alt="" />
+                        </div>
+                    </a>
+                    <div className="producto_footer">
+                        <h1>Title</h1>
+                        <p>Categoria</p>
+                        <p className="price">$250</p>
+                    </div>
+                    <div className="buttom">
+                        <button className="btn">Añadir</button>
+                        <div>
+                            <a href="y" className="">
+                                Vista
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>        
+        
         </>
-    )     
-}
+    );
+};
